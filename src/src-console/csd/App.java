6bbc4d4e31bd2d0c0e2a71cd1ac390ead1354770
Oğuz Karan <oguzkaran@csystem.org>
2021-05-16 16:43:21 +0300

    Java-May-2021

--- src/src-console/csd/App.java
+++ src/src-console/csd/App.java
@@ -1,8 +1,36 @@
+/*----------------------------------------------------------------------------------------------------------------------
+	Çağıran metot ile çağrılan metot aynı sınıftaysa ise bu durumda paket ismi yazılmamışsa sınıf ismi de yazılmayabilir
+----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
-	public static void main(String[] args)
+	public static void main(String [] args) 
 	{
-		System.out.println("Hello, World");		
+		System.out.println("Hello World");
+		Sample.foo();
+		Sample.bar();
+		Mample.tar();
+		System.out.println("Goodbye World");
+	}
+}
+
+class Sample {
+	public static void foo() 
+	{
+		System.out.println("foo");
+		Mample.tar();
+	}
+
+	public static void bar()
+	{
+		System.out.println("bar");
+		foo();
+	}
+}
+
+class Mample {
+	public static void tar()
+	{
+		System.out.println("tar");
 	}
 }
