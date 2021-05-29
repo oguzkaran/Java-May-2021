afc296c0f0ea8c956f353978b1fe32195a6993a5
Oğuz Karan <oguzkaran@csystem.org>
2021-05-29 16:34:50 +0300

    Java-May-2021

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,28 +1,26 @@
-/*----------------------------------------------------------------------------------------------------------------------
-	void metotlar geri dönüş değeri varmış gibi çağrılamaz. Yani void metot çağrısı geri dönüş değeri varmış gibi
-	işleme sokulamaz
+/*----------------------------------------------------------------------------------------------------------------------	
+	Anahtar Notlar: Gerçek sayılar ile bölme işleminde payda sıfır olsa da işlem yapılır. Gerçek sayılar içerisinde
+	genel olarak "belirsizlik" için kullanılan "Not a number (NaN)" değeri vardır. Ayrıca sonsuzluk (ya da tanımsızlık)
+	için kullanılan +Infinity ve -Infinity değerleri de vardır. Bu durumda gerçek sayılar ile bölme işleminde pay sıfırdan
+	farklı payda sıfırsa payın işaretine göre + veya - Infinity, pay ve paydanın ikisi birden sıfır ise NaN sonucu
+	elde edilir
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		NumberUtil.displaySquare();
-		NumberUtil.displaySquare();
-	}
-}
-
-
-class NumberUtil {
-	public static void displaySquare()
-	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Bir sayı giriniz:");
-		int val = Integer.parseInt(kb.nextLine());	
+		System.out.print("Birinci sayıyı giriniz:");
+		double a = Double.parseDouble(kb.nextLine());
+		System.out.print("İkinci sayıyı giriniz:");
+		double b = Double.parseDouble(kb.nextLine());
 		
-		System.out.println(val * val);
-		//...
+		double result;
+		
+		result = a / b;
+		
+		System.out.printf("%f / %f = %f%n", a, b, result);
 	}
 }
-
 
