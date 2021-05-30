c0863f385010a4ccbdc3a3c66807ad3f745a4836
Oğuz Karan <oguzkaran@csystem.org>
2021-05-30 16:36:18 +0300

    Java-May-2021

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,43 +1,62 @@
 /*----------------------------------------------------------------------------------------------------------------------	
-	Sınıf Çalışması: Parametresi ile aldığı double türden x1, y1, x2, y2 koordinat bilgilerine göre iki nokta
-	arasındaki  ((x1, y1) ve (x2, y2)) uzaklığa geri dönen distance isimli metodu PointUtil isimli sınıf içerisinde 
-	yazınız	ve klavyeden girilen değerlerle çağırarak test ediniz
+	Sabitler:
+	Program içerisinde doğrudan yazılan sayılara denir. Sabitlerin de türleri vardır. Derleyici sabitlerin türlerini
+	tespit ederek işlem yapar. 
+	Sabitlerin türlerinin tespitine ilişkin detaylar:
+	- Sayı nokta içermiyorsa ve sonuna bir ek almamışsa ve int türü sınırları içerisindeyse int türdendir. Aksi durumda
+	eror oluşur
+	Örnek: 1000, 2000000000, 3
+	
+	- Sayı nokta içermiyorsa ve sonuna L (küçük veya büyük) ekini almışsa long türdendir. long türü sınırları dışında ise
+	error oluşur.
+	Örneğin: 10L, 4000000000L
+	
+	Anahtar Notlar: Küçük harf L kullanımı 1(bir) rakamına benzemesinden dolayı programlamada tercih edilmez
+	
+	- Java'da byte ve short türden sabitler yoktur
+	
+	- Sayı nokta içeriyorsa ve sonuna bir ek almamışsa double türdendir. Sayı double olarak ifade edilemiyorsa error oluşur.
+	Örneğin:3.4, 6.7
+	
+	- Sayı nokta içersin ya da içermesin sonunda D (küçük veya büyük) eki varsa double türdendir. 
+	Örnek: 3D, 3d, 3.4D
+	
+	- Sayı nokta içersin ya da içermesin sonunda F (küçük veya büyük) eki varsa float türdendir. 
+	Örnek: 3f, 3F, 3.4F
+	
+	- boolean türden iki tane sabit vardır: true, false
+	
+	- char türden sabitler: İki tek tırnak karakteri yazılan bir karakter karakter tablosunda sıra numarası belitir. Bu 
+	şekilde yazılan sabitlere karakter sabitleri denir ve bu sabitler char türdendir. İki tane tek tırnak arasında 
+	özel durumlar dışında birden fazla karakter yazımı geçersizdir. Örneğin 'ş' karakter sabiti karakter tablosundaki
+	sıra numarasına karşılık gelir. 
+	
+	Bazı karakterlerin klavyeden karakter olarak girilmesi dorğudan mümkkün değildir. Örneğin bir sonraki satıra geçme
+	karakterinin karakter sabiti klavyeden bir karakter olarak basılamaz. Bazı karakterlerin görüntü karşılığı da yoktur.
+	Bunlar gibi bazı karakterler de klavyeden doğrudan yazılamazalar. Bu tarz karakterlerin karakter sabitleri iki 
+	tane tek tırnak arasında ters bölü karakteri ile onu takip eden özel bir karakter ile yazılır. Bu şekilde yazılan
+	karakterlere "escape sequence" karakterler denir. Hatta bazı karakteler hem normal hem de ters bölü yazılabilirler.
+	Java'da desteklenen escape sequence karakterler şunlardır:
+	'\n' -> line feed (LF)
+	'\r' -> carriage return (CR)
+	'\t' -> horizontal tab
+	'\b' -> backspace
+	'\f' -> form feed
+	'\0' -> null karakter
+	...
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		DistanceTest.run();		
+		char c;
+		
+		c = '\f';
+		
+		System.out.printf("c = %c%n", c);
+		
 	}
 }
 
-class DistanceTest {
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("x1?:");
-		double x1 = Double.parseDouble(kb.nextLine());
-		
-		System.out.print("y1?:");
-		double y1 = Double.parseDouble(kb.nextLine());
-		
-		System.out.print("x2?:");
-		double x2 = Double.parseDouble(kb.nextLine());
-		
-		System.out.print("y2?:");
-		double y2 = Double.parseDouble(kb.nextLine());
-		
-		System.out.printf("Distance:%f%n", PointUtil.distance(x1, y1, x2, y2));		
-	}
-}
 
-class PointUtil {
-	public static double distance(double x1, double y1, double x2, double y2)
-	{		
-		double diffX = x1 - x2;
-		double diffY = y1 - y2;
-		
-		return Math.sqrt(diffX * diffX + diffY * diffY);		
-	}
-}
