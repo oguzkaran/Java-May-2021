b76f5e57fae3a8b5aa29a0133f02b12582e0b58d
Oğuz Karan <oguzkaran@csystem.org>
2021-06-06 17:05:48 +0300

    Java-May-2021

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,20 +1,38 @@
 /*----------------------------------------------------------------------------------------------------------------------	
-	Gerçek sayı sabitlerinin üstel (bilimsel) gösterilişi
+	Aşağıdaki örnekte işlem sırası ile operatörlerinm öncelik sırası aynıdır. Yine en soldaki yapılır.
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{		
-		double a = 1.3E10;
-		double b = 1.3e10;
-		double c = 1.3E-10;
-		double d = 1.3e-10;
+	{	
+		boolean result;
 		
+		result = Sample.bar() || Sample.foo() && Sample.tar();
 		
-		System.out.printf("a = %f%n", a);
-		System.out.printf("b = %f%n", b);
-		System.out.printf("c = %.12f%n", c);
-		System.out.printf("d = %.12f%n", d);
+		System.out.printf("result = %b%n", result);
+	}
+}
+
+class Sample {
+	public static boolean foo()
+	{
+		System.out.println("foo");
+		
+		return true;
+	}
+	
+	public static boolean bar()
+	{
+		System.out.println("bar");
+		
+		return false;
+	}
+	
+	public static boolean tar()
+	{
+		System.out.println("tar");
+		
+		return false;
 	}
 }
