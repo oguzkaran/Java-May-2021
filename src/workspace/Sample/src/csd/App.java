f1748e63fd93731a37241802ad6eea6a80d52f44
Oğuz Karan <oguzkaran@csystem.org>
2021-07-11 16:33:40 +0300

    Java-May-2021

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,25 +1,46 @@
 /*---------------------------------------------------------------------------------------------------------------------- 	 
- 	Sınıf bildirimi bir tür bildirimidir (user defined type)
+ 	Homework-003-8. sorunun bir çözümü 	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
+		DisplayCollatzTest.run();
+	}
+}
+
+class DisplayCollatzTest {	
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);		
 		
+		for (;;) {
+			System.out.print("Pozitif bir sayı giriniz:");
+			int val = Integer.parseInt(kb.nextLine());
+			
+			if (val == 0)
+				break;
+				
+			NumberUtil.displayCollatz(val);			
+		}
+	}
+}
+
+class NumberUtil {	
+	public static void displayCollatz(int n)
+	{
+		if (n <= 0) {
+			System.out.println("Geçersiz sayı");
+			return;
+		}
+		
+		System.out.println(n);
+		
+		while (n != 1)			
+			System.out.println(n = n % 2 == 0 ? n / 2 : 3 * n + 1);	
 	}
 }
 
 
-class Device {
-	//...
-}
-
-class SerialPort {
-	//...
-}
-
-class Person {
-	//...
-}
 
