f270b364d09cac83342a16e0225cd99fccc0ac2c
Oğuz Karan <oguzkaran@csystem.org>
2021-07-24 14:39:55 +0300

    Java-May-2021

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,45 +1,37 @@
 /*---------------------------------------------------------------------------------------------------------------------- 	 
- 	Homework-003-8. sorunun bir çözümü 	
+	Sınıfın non-static veri elemanlarına sınıf dışından (başka bir sınıfın içinden) referans ve nokta operatörü ile
+	erişilebilir. Sınıfın non-static veri elemanları her nesne için ayrıca nesnenin içerisinde yaratılırlar. İşte sınıf
+	dışından referans ile erişilen eleman o referansın gösterdiği nesnenin elemanıdır. Her new işlemi yeni bir nesne
+	yaratılmasına yol açar. Aşağıdaki kodu çalıştırıp sonucu gözlemleyiniz 
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{
-		DisplayCollatzTest.run();
+		Sample s, k;
+		
+		s = new Sample();
+		k = new Sample();
+		
+		s.a = 10;
+		s.b = true;
+		
+		k.a = 20;
+		k.b = false;
+		
+		System.out.printf("s.a = %d%n", s.a);
+		System.out.printf("s.b = %b%n", s.b);
+		
+		System.out.printf("k.a = %d%n", k.a);
+		System.out.printf("k.b = %b%n", k.b);
 	}
 }
 
-class DisplayCollatzTest {	
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);		
-		
-		for (;;) {
-			System.out.print("Pozitif bir sayı giriniz:");
-			int val = Integer.parseInt(kb.nextLine());
-			
-			if (val == 0)
-				break;
-				
-			NumberUtil.displayCollatz(val);			
-		}
-	}
-}
-
-class NumberUtil {	
-	public static void displayCollatz(int n)
-	{
-		if (n <= 0) {
-			System.out.println("Geçersiz sayı");
-			return;
-		}
-		
-		System.out.println(n);
-		
-		while (n != 1)			
-			System.out.println(n = n % 2 == 0 ? n / 2 : 3 * n + 1);	
-	}
+class Sample {
+	public int a;
+	public boolean b;
+	//...
 }
 
 
