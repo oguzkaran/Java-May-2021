b547d41f4b6d6064ca60a20ce94fcededa20ed74
Oğuz Karan <oguzkaran@csystem.org>
2021-07-25 14:33:17 +0300

    Java-May-2021

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,44 +1,18 @@
 /*---------------------------------------------------------------------------------------------------------------------- 	 
-	 Referans parametreli metotlar yazılabilir. Metot içerisinde referans parametresine geçilen adrese ilişkin
-	 nesneye erişilebilir. Nesne üzerinde değişiklik yapılabilir
+	 Sınıfın non-static bir veri elemanına sınıf dışından sınıf ismi ve nokta operatörü ile erişilemez
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{	
-		Date birthDate = new Date();
-		
-		birthDate.day = 10;
-		birthDate.month = 9;
-		birthDate.year = 1976;
-		
-		DateUtil.display(birthDate);
-		
-		DateUtil.changeDate(birthDate, 11, 7, 1983);
-		
-		DateUtil.display(birthDate);
-		
-		System.out.println("Tekrar yapıyor musunuz?");
-	}
-}
-
-class DateUtil {
-	public static void changeDate(Date d, int day, int month, int year)
 	{
 		//...
-		d.day = day;
-		d.month = month;
-		d.year = year;
-	}
-	
-	public static void display(Date d)
-	{
-		System.out.printf("%02d/%02d/%04d%n", d.day, d.month, d.year);
+		Sample.a = 10; //error		
+		Sample.b = true; //error
 	}
 }
 
-class Date {
-	public int day, month, year;
-	//...
+class Sample {
+	public int a;
+	public boolean b;
 }
