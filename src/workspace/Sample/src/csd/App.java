d9eb05e75566da768c7bb78950e78e9b509d26e8
Oğuz Karan <oguzkaran@csystem.org>
2021-07-31 16:46:00 +0300

    Java-May-2021

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,24 +1,31 @@
 /*----------------------------------------------------------------------------------------------------------------------	
-	 Random sınıfının nextDouble metodu [min, max) aralığında rasgele double türden sayı üretilmesi	 
+	 Aşağıdaki örnekte rasgele üretilen ilk 10 sayının dizilimi her çalıştırmada farklı olur. Dizilimin aynı gelme 
+	 olasılığı çok düşüktür. İkinci üretilen 10 sayı klavyeden alınan tohum değerine göre üretilir. Bu durumda örneğin
+	 her çalıştırmada tohum değeri 100 girildiğinde ikinci 10 sayının dizilimi aynı olacaktır. Kodu çalıştırıp
+	 sonucu gözlemleyiniz 	   
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{			
-		java.util.Random r = new java.util.Random();
+	{		
 		java.util.Scanner kb = new java.util.Scanner(System.in);
+		java.util.Random r = new java.util.Random();		
 		
-		System.out.print("Birinci sayıyı giriniz:");
-		double min = Double.parseDouble(kb.nextLine());
-		
-		System.out.print("İkinci sayıyı giriniz:");
-		double max = Double.parseDouble(kb.nextLine());
-		
+		System.out.print("Bir sayı giriniz:");
+		long seed = Long.parseLong(kb.nextLine());
 		
 		for (int i = 0; i < 10; ++i)
-			System.out.printf("%.10f%n", r.nextDouble() * (max - min) + min); //[mi, max)	
+			System.out.printf("%02d ", r.nextInt(100));
+		
+		System.out.println();
+		
+		r.setSeed(seed);
+		
+		for (int i = 0; i < 10; ++i)
+			System.out.printf("%02d ", r.nextInt(100));
+		
+		System.out.println();
 	}
 }
-
 
