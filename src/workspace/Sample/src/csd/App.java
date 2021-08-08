5059f75931ba4e3f9033f15b47ff81c22a7b3112
Oğuz Karan <oguzkaran@csystem.org>
2021-08-08 16:32:48 +0300

    Java-May-2021

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,91 +1,25 @@
 /*----------------------------------------------------------------------------------------------------------------------	
-	Sınıf Çalışması: Klavyeden bir kullanıcı adı ve şifre isteyen basit bir ATM uygulamasının bir parçasını aşağıdaki
-	açıklamalara göre yazınız.
-	
-	 Açıklamalar:
-	 - Kullanıcı ad ve şifresi en fazla 3(üç) kez denenebilecektir.
-	 
-	 - Denemeler sırasında doğru giriş yapılırsa "Giriş başarılı", başarısız denemelerde "Giriş başarısız", 3. denemede de
-	 başarısız olunursa "Giriş başarısız . Artık deneme hakkınız bitti" mesajla verilecektir.
-	 
-	 - Kullanıcı adı ve şifrenin doğruluğu program içerisinde belirlenen bir kullanıcı adı ve şifre ile yapılacaktır 
-	 
-	 - Örnekte ATM uygulaması sürekli çalışmaktadır
+	Aşağıdaki örneği inceleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{		
-		ATMApp.run(3);
-	}
-}
-
-class ATMApp {
-	public static void doWorkForSuccess(ATMCard card, java.util.Scanner kb)
-	{
-		System.out.println("Giriş başarılı");
-		//...
-	}
-	
-	public static void doWorkForFail(ATMCard card, java.util.Scanner kb)
-	{
-		System.out.println("Giriş başarısız . Artık deneme hakkınız bitti");
-		//...
-	}
-	
-	public static void doWorkForCard(ATMCard card, int tryNum, java.util.Scanner kb)
-	{
-		int i = 0;
-		
-		for (; i < tryNum; ++i) {
-			System.out.print("Kullanıcı adını giriniz:");
-			String username = kb.nextLine();
-			System.out.print("Şifreyi giriniz:");
-			String password = kb.nextLine();
-			
-			if (card.isValid(username, password))
-				break;	
-			
-			if (i != tryNum - 1)
-				System.out.printf("Giriş başarısız. %d giriş hakkınız kaldı%n", tryNum - 1 - i);
-		}		
-		
-		if (i != tryNum)
-			doWorkForSuccess(card, kb);
-		else
-			doWorkForFail(card, kb);
-	}
-	
-	public static void run(int tryNum)
-	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Email adresini giriniz:");
+		String email = kb.nextLine();
 		
-		for (;;) {
-			System.out.println("Hoşgeldiniz!...");
-			ATMCard atmCard = new ATMCard();
+		int atIdx = email.indexOf('@');
+		
+		if (atIdx != -1) {
+			if (email.substring(atIdx).lastIndexOf('.') == -1)
+				email += ".com";
 			
-			doWorkForCard(atmCard, tryNum, kb);			
+			System.out.println(email);				
 		}
+		else 
+			System.out.println("Geçersiz email");
 	}
 }
 
-class ATMCard {
-	public String username;
-	public String password;
-	//...
-	
-	public ATMCard()
-	{
-		//Burada kartın içinden okunuyor olarak düşüneilirsiniz
-		username = "csd";
-		password = "1234";			
-	}
-	
-	public boolean isValid(String uname, String passwd)
-	{
-		return uname.equals(username) && passwd.equals(password);
-	}
-	
-	//...
-}
