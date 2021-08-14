711bb88066947c5fdf0d542f14798483056785e8
Oğuz Karan <oguzkaran@csystem.org>
2021-08-14 15:23:45 +0300

    Java-May-2021

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,25 +1,85 @@
 /*----------------------------------------------------------------------------------------------------------------------	
-	Aşağıdaki örneği inceleyiniz
+	Sınıf Çalışması: Aşağıda açıklanan metotları yazınız.
+	
+	public static String padLeading(String s, int length, char ch);
+	public static String padLeading(String s, int length);
+	public static String padTrailing(String s, int length, char ch);
+	public static String padTrailing(String s, int length);
+	
+	Açıklamalar:
+	- padLeading metodunun 3 parametreli overload'u ikinci parametresi ile aldığı length birinci parametresi ile
+	aldığının uzunluğundan büyükse yazıyı soldan üçüncü parametresi ile aldığı karakter ile doldurulmuş yeni bir
+	String referansına geri dönecektir.
+		Örneğin:
+		padLeading("ankara", 11, 'x'); çağrısı
+		xxxxxankara 
+		yazısını döndürecektir
+	
+	- length uzunluğu yazının uzunluğundan küçük veya eşitse aynı referansa geri dçnecektir.
+	- padLeading metodunun 2 parametreli overload'u yazıyı soldan space karakteri ile dolduracaktır
+	- padTrailin metotşarı benzer şekilde sağdan dolduracak şekilde yazılacaktır 
+	- Örneği Java 11+ için yazınız		
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
 	{		
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Email adresini giriniz:");
-		String email = kb.nextLine();
-		
-		int atIdx = email.indexOf('@');
-		
-		if (atIdx != -1) {
-			if (email.substring(atIdx).lastIndexOf('.') == -1)
-				email += ".com";
-			
-			System.out.println(email);				
-		}
-		else 
-			System.out.println("Geçersiz email");
+		PadLeadingTrailingTest.run();	
 	}
 }
+
+
+class PadLeadingTrailingTest {
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		for (;;) {
+			System.out.print("Bir yazı giriniz:");
+			String s = kb.nextLine();			
+			
+			if ("elma".equals(s))
+				break;
+			
+			System.out.print("Bir sayı giriniz:");
+			int newLength = Integer.parseInt(kb.nextLine());			
+			
+			
+			System.out.printf("(%d)%n", StringUtil.padLeading(s, newLength, 'x'));
+			System.out.printf("(%d)%n", StringUtil.padLeading(s, newLength));
+			System.out.printf("(%d)%n", StringUtil.padTrailing(s, newLength, 'x'));			
+			System.out.printf("(%d)%n", StringUtil.padTrailing(s, newLength));			
+		}
+		
+		System.out.println("Tekrar yapıyor musunuz?");		
+	}
+}
+
+class StringUtil {
+	public static String padLeading(String s, int length, char ch)
+	{
+		
+	}
+	
+	public static String padLeading(String s, int length)
+	{
+		
+	}
+	
+	public static String padTrailing(String s, int length, char ch)
+	{
+		
+	}
+	
+	public static String padTrailing(String s, int length)
+	{
+		
+	}
+
+}
+
+
+
+
 
