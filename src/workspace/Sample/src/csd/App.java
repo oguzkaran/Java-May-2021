2e362b7d8203d74b19b4d5c6218201cc48cd2a0f
Oğuz Karan <oguzkaran@csystem.org>
2021-08-14 16:40:52 +0300

    Java-May-2021

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,85 +1,27 @@
-/*----------------------------------------------------------------------------------------------------------------------	
-	Sınıf Çalışması: Aşağıda açıklanan metotları yazınız.
-	
-	public static String padLeading(String s, int length, char ch);
-	public static String padLeading(String s, int length);
-	public static String padTrailing(String s, int length, char ch);
-	public static String padTrailing(String s, int length);
-	
-	Açıklamalar:
-	- padLeading metodunun 3 parametreli overload'u ikinci parametresi ile aldığı length birinci parametresi ile
-	aldığının uzunluğundan büyükse yazıyı soldan üçüncü parametresi ile aldığı karakter ile doldurulmuş yeni bir
-	String referansına geri dönecektir.
-		Örneğin:
-		padLeading("ankara", 11, 'x'); çağrısı
-		xxxxxankara 
-		yazısını döndürecektir
-	
-	- length uzunluğu yazının uzunluğundan küçük veya eşitse aynı referansa geri dçnecektir.
-	- padLeading metodunun 2 parametreli overload'u yazıyı soldan space karakteri ile dolduracaktır
-	- padTrailin metotşarı benzer şekilde sağdan dolduracak şekilde yazılacaktır 
-	- Örneği Java 11+ için yazınız		
+/*---------------------------------------------------------------------------------------------------------------------- 	 
+ 	 Genel olarak söylemek gerekirse Scanner'ın çalışma sistematiği dolayısıyla nextInt, nextLong ve nextDouble 
+ 	 gibi metotlar aynı Scanner nesnesine ilişkin nextLine metodu akışta çağrılmamalıdır. Bu yüzden kalvye işlemlerinde
+ 	 nextLine metodu dıiımdakileri tercihetmiyoruz. Aşağıdaki programı çalıştırarak sonucu gözlemleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
-	{		
-		PadLeadingTrailingTest.run();	
-	}
-}
-
-
-class PadLeadingTrailingTest {
-	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
 		for (;;) {
-			System.out.print("Bir yazı giriniz:");
-			String s = kb.nextLine();			
+			System.out.print("Öğrenci numarasını giriniz:");
+			int number = kb.nextInt();
 			
-			if ("elma".equals(s))
+			if (number == 0)
 				break;
 			
-			System.out.print("Bir sayı giriniz:");
-			int newLength = Integer.parseInt(kb.nextLine());			
+			System.out.print("Öğrencinin ismini giriniz:");
+			String name = kb.nextLine();
 			
-			
-			System.out.printf("(%d)%n", StringUtil.padLeading(s, newLength, 'x'));
-			System.out.printf("(%d)%n", StringUtil.padLeading(s, newLength));
-			System.out.printf("(%d)%n", StringUtil.padTrailing(s, newLength, 'x'));			
-			System.out.printf("(%d)%n", StringUtil.padTrailing(s, newLength));			
-		}
-		
-		System.out.println("Tekrar yapıyor musunuz?");		
+			System.out.printf("%d, %s%n", number, name);
+		}		
 	}
 }
-
-class StringUtil {
-	public static String padLeading(String s, int length, char ch)
-	{
-		
-	}
-	
-	public static String padLeading(String s, int length)
-	{
-		
-	}
-	
-	public static String padTrailing(String s, int length, char ch)
-	{
-		
-	}
-	
-	public static String padTrailing(String s, int length)
-	{
-		
-	}
-
-}
-
-
-
-
 
