cf056009081c8034536301af5ea035ebd369f90c
Oğuz Karan <oguzkaran@csystem.org>
2021-08-15 16:48:43 +0300

    Java-May-2021

--- src/workspace/Sample/src/csd/App.java
+++ src/workspace/Sample/src/csd/App.java
@@ -1,27 +1,75 @@
 /*---------------------------------------------------------------------------------------------------------------------- 	 
- 	 Genel olarak söylemek gerekirse Scanner'ın çalışma sistematiği dolayısıyla nextInt, nextLong ve nextDouble 
- 	 gibi metotlar aynı Scanner nesnesine ilişkin nextLine metodu akışta çağrılmamalıdır. Bu yüzden kalvye işlemlerinde
- 	 nextLine metodu dıiımdakileri tercihetmiyoruz. Aşağıdaki programı çalıştırarak sonucu gözlemleyiniz
+ 	 Sınıf Çalışması: Parametresi ile aldığı bir yazının palindrom olup olmadığını test eden isPalindrome metodunu
+ 	 yazınız ve aşağıdaki kod ile test ediniz. 
+ 	 Palindrom: Yalnızca alfabetik karakterleri tersten okunduğunda aynı olan yazılara palindrom denir. Detaylar 
+ 	 gözardı edilmesi koşuluyla bu tanım yazılabilir.
+ 	 
+ 	 Örnek: 
+ 	 Ey Edip Adana'da pide ye 	-> eyedipadanadapideye
+ 	 Anastas mum satsana 		-> anastasmumsatsana
+ 	 Ali Papila					-> alipapila
+ 	 
+ 	 Açıklama: Örnek daha önce yazdığımız reverse metodu kullanılarak çok daha basit biçimde yazılabilirdi. Ancak 
+ 	 daha önce de belirtildiği gibi reverse metodunun ilgili versiyonu sürekli String nesnesi yarattığından böyle bir
+ 	 durumda tercih edilmesi uygun değildir. Ancak ileride reverse metodunu daha etkin hale getirdiğimizde kodu basitleştirmesi
+ 	 açısından isPalindrome metodunda reverse kullanabileceğiz
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args) 
+	{	
+		IsPalindromeTest.run();
+	}
+}
+
+class IsPalindromeTest {
+	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
 		for (;;) {
-			System.out.print("Öğrenci numarasını giriniz:");
-			int number = kb.nextInt();
+			System.out.print("Bir sayı giriniz:");
+			String s = kb.nextLine();
 			
-			if (number == 0)
+			if ("elma".equals(s))
 				break;
 			
-			System.out.print("Öğrencinin ismini giriniz:");
-			String name = kb.nextLine();
-			
-			System.out.printf("%d, %s%n", number, name);
-		}		
+			System.out.println(StringUtil.isPalindrome(s) ? "Palindrom" : "Palindrom değil");
+		}
 	}
 }
+
+class StringUtil {	
+	public static boolean isPalindrome(String s)
+	{
+		int left = 0;
+		int right = s.length() - 1;
+		
+		while (left < right) {
+			char cLeft = Character.toLowerCase(s.charAt(left));
+			
+			if (!Character.isLetter(cLeft)) {
+				++left;
+				continue;
+			}
+			
+			char cRight = Character.toLowerCase(s.charAt(right));
+			
+			if (!Character.isLetter(cRight)) {
+				--right;
+				continue;
+			}
+			
+			if (cLeft != cRight)
+				return false;
+			
+			++left;
+			--right;
+		}
+		
+		return true;
+	}
+}
+
 
