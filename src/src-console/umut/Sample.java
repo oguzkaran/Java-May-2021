75e1aace9570999c669645a8b53055b9fd032851
Oğuz Karan <oguzkaran@csystem.org>
2021-08-22 16:49:17 +0300

    Java-May-2021

--- src/src-console/umut/Sample.java
+++ src/src-console/umut/Sample.java
@@ -1,13 +1,12 @@
 /*----------------------------------------------------------------------------------------------------------------------
 	Çağıran metot ile çağrılan metot aynı sınıftaysa ise bu durumda paket ismi yazılmamışsa sınıf ismi de yazılmayabilir
 ----------------------------------------------------------------------------------------------------------------------*/
-package csd;
+package umut;
 
-class App {
+public class Sample {
 	public static void main(String [] args) 
 	{		
-		System.out.println("Merhaba ben csd");	
-		umut.Sample.main(args);	
+		System.out.println("Merhaba ben umut");		
 	}
 }
 
---
+++ src/workspace/Sample/src/org/csystem/util/number/NumberUtil.java
@@ -0,0 +1,5 @@
+package org.csystem.util.number;
+
+public class NumberUtil {
+	
+}
