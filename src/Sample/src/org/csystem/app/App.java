ffd2da59f40977865d68889021502054ea1e6caa
Oğuz Karan <oguzkaran@csystem.org>
2021-09-11 13:45:22 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,16 +1,37 @@
 /*---------------------------------------------------------------------------------------------------------------------- 	 
-	Aşağıdaki durumda referansa ildeğer verilmediği diziye ilkdeğer verirken new operatörü kullanılmalıdır
+	Sınıf Çalışması: Parametresi ile aldığı int türden bir dizinin elemanlarının toplamını döndüren sum isimli metodu
+	ArrayUtil sınıfı içerisinde yazınız ve aşağıdaki kod ile test ediniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import java.util.Scanner;
 
 class App {
 	public static void main(String [] args) 
 	{
-		int [] a;
-		a = {1, 2, 3, 4, 5, 6, 9, 7 }; //error
+		SumIntArrayTest.run();
+	}
+}
 
-		for (int i = 0; i < a.length; ++i)
-			System.out.printf("%d ", a[i]);
+class SumIntArrayTest {
+	public static void run()
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.print("Min değerini giriniz:");
+		int min = Integer.parseInt(kb.nextLine());
+
+		System.out.print("Max değerini giriniz:");
+		int max = Integer.parseInt(kb.nextLine());
+
+		for (;;) {
+			System.out.print("Dizinin eleman sayısını giriniz:");
+			int n = Integer.parseInt(kb.nextLine());
+
+			if (n <= 0)
+				break;
+		}
+
+		System.out.println("Tekrar yapıyor musunuz?");
 	}
 }
 
---
+++ src/Sample/src/org/csystem/util/array/ArrayUtil.java
@@ -0,0 +1,49 @@
+package org.csystem.util.array;
+
+import java.util.Random;
+
+public class ArrayUtil {
+    public static void display(int [] a)
+    {
+        for (int i = 0; i < a.length; ++i)
+            System.out.printf("%d ", a[i]);
+
+        System.out.println();
+    }
+
+    public static void display(double [] a)
+    {
+        for (int i = 0; i < a.length; ++i)
+            System.out.printf("%f", a[i]);
+
+        System.out.println();
+    }
+
+    public static int [] getRandomArray(int n, int min, int max) //[min, max]
+    {
+        return getRandomArray(new Random(), n, min, max);
+    }
+
+    public static int [] getRandomArray(Random r, int n, int min, int max) //[min, max]
+    {
+        int [] a = new int[n];
+
+        for (int i = 0; i < n; ++i)
+            a[i] = r.nextInt(max - min + 1) + min;
+
+        return a;
+    }
+
+    public static int sum(int [] a)
+    {
+
+    }
+
+    public static void swap(int [] a, int i, int k)
+    {
+        int temp = a[i];
+
+        a[i] = a[k];
+        a[k] = temp;
+    }
+}
