083c236f9f46d938b6349558c969b145dcdd0c2b
Oğuz Karan <oguzkaran@csystem.org>
2021-10-09 14:31:45 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,23 +1,6 @@
 /*----------------------------------------------------------------------------------------------------------------------
- 	Aşağıdaki durumlardan en az biri varsa veri elemanının gizlenmesi gerekir:
- 	- Sınıfın geliştirilmesinde versiyon ilerledikçe bir veri elemanının isminin hatta türünün değişmesine çok sık rastlanır.
- 	Bu durumda eski müşteri kodların bu değişimdn etkilenmemesi için veri elemanı gizlenmelidir
-
- 	- Bir veri elemanının sınır değerleri olabilir. Bu değerlerin kontrol edilip değiştirilebilmesi için veri elemanı
- 	gizlenmelidir
-
- 	- Bir veri elemanının değer değiştiğinde başka bir veri elemanının değerinin değişen elemana göre tekrar belirlenmesi
- 	gerekebilir. Bu durumda da veri elemanı gizlenmelidir
-
- 	- Bir veri elemanı değiştiğinde yeni değere göre bazı işlemlerin yapılması gerekebilir. Örneğin, bir dosya ismini
- 	alan bir sınıf bu isme göre dosyayı "açar". Açılan bir dosyanın "kapatılması" gerekir. O nesne için dosya ismi değiştiğinde
- 	eski dosyanın kapatılıp yeni dosyanın açılması gerekir. Bu durumda da veri elemanı gizlenmelidir.
-
- 	- Bazı durumlarda veri elemanının gizlenmmesi gerekmese bile gizleyebilir. Örneğin sınıfın tüm veri elemanları bir ya da
- 	birkaç sebepten dolayı gizlenmiş olabilir. Ancak bir veri elemanı yukarıda yazılan durumların hiçbirisi olmayabilir.
- 	Bu durumda diğer elemanlar da gizlendiği için gizlenmesi uygun olabilir.
-
- 	Bu durumlar dışında (%3 ile %5 arasında) sınıfın veri elemanları public yapılabilir
+ 	Aşağıdaki örnekte Date sınıfının içsel tasarımı ve algoritması değişmesine rağmen müşteri kodlar bundan etkilenmemiştir.
+ 	Detayları gözardı ederek sadece değişten sonra müşteri kodların etkilenmemesine odaklanınız
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
@@ -26,70 +9,65 @@ class App {
 	{
 		Date birthDate = new Date(6, 9, 2021);
 
-		System.out.printf("%02d/%02d/%04d%n", birthDate.getDay(), birthDate.getMonth(), birthDate.getYear());
+		System.out.printf("%02d/%02d/%04d%n", birthDate.getDay(), birthDate.getMonthValue(), birthDate.getYear());
 
 		birthDate.setDay(11);
-		birthDate.setMonth(7);
+		birthDate.setMonthValue(7);
 		birthDate.setYear(1983);
 
-		System.out.printf("%02d/%02d/%04d%n", birthDate.getDay(), birthDate.getMonth(), birthDate.getYear());
+		System.out.printf("%02d/%02d/%04d%n", birthDate.getDay(), birthDate.getMonthValue(), birthDate.getYear());
 	}
 }
 
 class Date {
-	private int m_day, m_month, m_year;
+	private String m_dateStr;
 	private int m_dayOfWeek;
-	//...
 
 	public Date(int day, int month, int year)
 	{
 		//...
-		m_day = day;
-		m_month = month;
-		m_year = year;
-		//...
+		m_dateStr = String.format("%02d/%02d/%04d", day, month, year);
+		//calculate dayOfWeek
 	}
 
 	public int getDay()
 	{
-		return m_day;
+		return Integer.parseInt(m_dateStr.substring(0, 2));
 	}
 
 	public void setDay(int val)
 	{
 		//...
-		m_day = val;
-		//...
+		m_dateStr = String.format("%02d/%02d/%04d", val, getMonthValue(), getYear());
+		//calculate dayOfWeek
+	}
+	
+	public int getMonthValue()
+	{
+		return Integer.parseInt(m_dateStr.substring(3, 5));
 	}
 
-	public int getMonth()
-	{
-		return m_month;
-	}
-
-	public void setMonth(int val)
+	public void setMonthValue(int val)
 	{
 		//...
-		m_month = val;
-		//...
+		m_dateStr = String.format("%02d/%02d/%04d", getDay(), val, getYear());
+		//calculate dayOfWeek
 	}
 
 	public int getYear()
 	{
-		return m_year;
+		return Integer.parseInt(m_dateStr.substring(6));
 	}
 
 	public void setYear(int val)
 	{
 		//...
-		m_year = val;
-		//...
+		m_dateStr = String.format("%02d/%02d/%04d", getDay(), getMonthValue(), val);
+		//calculate dayOfWeek
 	}
 
 	public int getDayOfWeek()
 	{
 		return m_dayOfWeek;
 	}
-
-	//...
 }
---
+++ src/Sample/src/org/csystem/util/math/geometry/Circle.java
@@ -0,0 +1,15 @@
+/*----------------------------------------------------------------------
+	FILE        : Circle.java
+	AUTHOR      : Java-May-2021 Group
+	LAST UPDATE : 09.10.2021
+
+	Circle class that represents the circle in geometry
+
+	Copyleft (c) 1993 by C and System Programmers Association (CSD)
+	All Rights Free
+-----------------------------------------------------------------------*/
+package org.csystem.util.math.geometry;
+
+public class Circle {
+    private int m_r;
+}
