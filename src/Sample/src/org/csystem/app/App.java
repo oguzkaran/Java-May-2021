1195d43f8bf2c1065a8fcba208897aa2cc972236
Oğuz Karan <oguzkaran@csystem.org>
2021-10-30 16:38:49 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,69 +1,118 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	A ile B arasındaki "composition" ilişkisinin genel biri implementasyonu
+	Taxi, Driver ve Client sınıfları ve aralarındaki ilişkiler
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 class App {
 	public static void main(String [] args)
 	{
-		Car car = new Car(/*...*/);
+		Taxi taxi = new Taxi(DriverUtil.getDriver()/*...*/);
+		Client client = ClientUtil.getClient();
 
-		car.run();
+		taxi.takeClient(client);
+		//...
 	}
 }
 
-class Car {
-	private Engine m_engine;
-	//...
-
-	public Car(/*...*/)
+class DriverUtil {
+	public static Driver getDriver()
 	{
-		//...
-		m_engine = new Engine(/*...*/);
-		//..
-	}
+		Driver driver = new Driver();
 
-	public void brake()
-	{
-		//...
-		m_engine.slowEngine();
-		//...
-	}
+		driver.setName("Oğuz Karan");
+		driver.setDriverLicenceNo(12345678);
 
-	public void run()
-	{
-		m_engine.startEngine();
-		m_engine.accelerateEngine();
-		//...
-		System.out.println("Running");
-		//...
-		brake();
-		m_engine.stopEngine();
+		return driver;
 	}
-	//....
 }
 
-class Engine {
+class ClientUtil {
+	public static Client getClient()
+	{
+		Client client = new Client();
+
+		client.setName("Çağıl Cebeci");
+		client.setUsername("ccebeci");
+
+		return client;
+	}
+}
+
+class Taxi {
+	private Driver m_driver;
 	//...
-	public void startEngine()
+	public Taxi(Driver driver)
 	{
-		System.out.println("Start engine");
+		m_driver = driver;
 	}
 
-	public void accelerateEngine()
+	public Driver getDriver()
 	{
-		System.out.println("Accelerate engine");
+		return m_driver;
 	}
 
-	public void slowEngine()
+	public void setDriver(Driver driver)
 	{
-		System.out.println("Slow engine");
-	}
-
-	public void stopEngine()
-	{
-		System.out.println("Stop engine");
+		//...
+		m_driver = driver;
 	}
 
 	//...
+	public void takeClient(Client client)
+	{
+		System.out.printf("Client Info:%s, %s%n",  client.getName(), client.getUsername());
+		System.out.printf("Driver Info:%s, %d%n", m_driver.getName(), m_driver.getDriverLicenceNo());
+	}
+}
+
+class Client {
+	private String m_username;
+	private String m_name;
+
+	//...
+	public String getUsername()
+	{
+		return m_username;
+	}
+
+	public void setUsername(String username)
+	{
+		m_username = username;
+	}
+
+	public String getName()
+	{
+		return m_name;
+	}
+
+	public void setName(String name)
+	{
+		m_name = name;
+	}
+}
+
+class Driver {
+	private String m_name;
+	private long m_driverLicenceNo;
+	//...
+
+	public String getName()
+	{
+		return m_name;
+	}
+
+	public void setName(String name)
+	{
+		m_name = name;
+	}
+
+	public long getDriverLicenceNo()
+	{
+		return m_driverLicenceNo;
+	}
+
+	public void setDriverLicenceNo(long driverLicenceNo)
+	{
+		m_driverLicenceNo = driverLicenceNo;
+	}
 }
