e50c70f79c535751c31e8f282c607014e33c23df
Oğuz Karan <oguzkaran@csystem.org>
2021-11-07 14:36:00 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,24 +1,51 @@
 /*----------------------------------------------------------------------------------------------------------------------
-
+	Taban sınıf türünden bir referans türemiş sınıf türünden bir referansa doğrudan atanamaz. Bu işlem tür dönüştürme
+	operatörü ile (explicit) yapılabilir (downcasting). Burada tür dönüştürme operatörü derlemeden geçmek için
+	kullanılır. Çalışma zamanında kaynak referansın dinamik türünün tür dönüştürme operatörüne yazılan türü kapsayıp
+	kapsamadığına bakılır. Kapsıyorsa haklı dönüşümdür, akış devam eder, kapsamıyorsa haksız dönüşümdür exception
+	oluşur. Buradaki kapsama nesnesel kapsamadır
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
-import org.csystem.application.company.*;
+import java.util.Scanner;
 
 class App {
 	public static void main(String [] args)
 	{
-		HumanResources humanResources = new HumanResources();
-		Manager m = new Manager();
-		Worker w = new Worker();
-		SalesManager sm = new SalesManager();
-		ProjectWorker pw = new ProjectWorker();
+		Scanner kb = new Scanner(System.in);
+		System.out.print("Bir sayı giriniz:");
+		int val = Integer.parseInt(kb.nextLine());
+		A x;
+		B y;
 
-		//...
+		x = val > 0 ? new B() : new A();
 
-		humanResources.payInsurance(m);
-		humanResources.payInsurance(w);
-		humanResources.payInsurance(sm);
-		humanResources.payInsurance(pw);
+		System.out.println(x.getClass().getName());
+
+		y = (B)x; //downcasting
+
+		y.a = 10;
+		Sample.foo(y);
+		System.out.println("---------------------------");
+		System.out.println(x.getClass().getName());
 	}
+}
+
+class Sample {
+	public static void foo(B x)
+	{
+		x.a = 20;
+		x.b = 30;
+	}
+}
+
+class B extends A {
+	public int b;
+	//...
+}
+
+class A {
+	public int a;
+
+	//...
 }
