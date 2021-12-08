30d96de37836ed748f9cde73259ac8f557db7ac4
Oğuz Karan <oguzkaran@csystem.org>
2021-12-08 18:48:15 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,12 +1,39 @@
 /*----------------------------------------------------------------------------------------------------------------------
-
+	Aşağıdaki örnekte C sınıfı için foo metodu public olarak override edilmelidir
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 class App {
 	public static void main(String [] args)
 	{
+		A a = new A();
+		IX ix = a;
 
+		ix.foo();
 	}
+}
+
+class C extends B implements IX { //error: Geçerli olsaydı IX için foo metodunun erişim belirleyicisi düşürülmüş olurdu
+
+}
+
+
+class A extends B implements IX {
+	protected void foo()
+	{
+		System.out.println("A.foo");
+	}
+}
+
+
+class B {
+	protected void foo()
+	{
+		System.out.println("B.foo");
+	}
+}
+
+interface IX {
+	void foo();
 }
 
