f88e48462f4302d1d7aeced8e242323beee6e335
Oğuz Karan <oguzkaran@csystem.org>
2021-12-12 20:23:56 +0300

    Update

--- src/Sample/src/org/csystem/application/io/file/BackupAndCopyApp.java
+++ src/Sample/src/org/csystem/application/io/file/BackupAndCopyApp.java
@@ -2,26 +2,11 @@
     Sınıf Çalışması: Komut satırından aşağıdaki gibi çalışan programı yazınız:
         java org.csystem.app.App <src> <dest>
     Program komut satırı argümanı verilmezse klavyeden yol ifadesini isteyecektir. src ile aldığı dosyayı dest ile
-    aldığı dosya olarak kopyalayacaktır. Hedef dosya varsa hedef dosya ismi ve uzantısının sonuna "-bak" ekelenerek
+    aldığı dosya olarak kopyalayacaktır. Hedef dosya varsa hedef dosya ismi ve uzantısının sonuna "-bak" eklenerek
     yedeklenecektir. Daha önce yedeklenmişse üzerine yazılacaktır. Bu işlemden sonra kopyalama yapılacaktır
 ----------------------------------------------------------------------------------------------------------------------*/
+package org.csystem.application.io.file;
 
-package org.csystem.app;
+public class BackupAndCopyApp {
 
-import java.io.FileOutputStream;
-
-import static org.csystem.util.console.CommandLineUtil.checkIfNotEqualAndExit;
-
-class App {
-	public static void main(String [] args)
-	{
-		checkIfNotEqualAndExit(args, 1, "Wrong number of arguments");
-
-		try (FileOutputStream fos = new FileOutputStream(args[0])) {
-			//...
-		}
-		catch (Throwable ex) {
-			System.err.println(ex.getMessage());
-		}
-	}
 }
