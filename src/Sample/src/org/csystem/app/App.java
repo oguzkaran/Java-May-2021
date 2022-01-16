23c4df894fad37610aaa17fce2e9de9c9a2e94b1
Oğuz Karan <oguzkaran@csystem.org>
2022-01-16 08:44:39 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -7,21 +7,6 @@ package org.csystem.app;
 class App {
 	public static void main(String [] args)
 	{
-		A<?> a;
 
-		a = new A<String>();
-
-		a.foo("ankara"); //error
-		a.bar();
 	}
 }
-
-enum A<T> { //error
-	//...
-}
-
-class MyException<T> extends RuntimeException { //error
-	//...
-}
-
-c
